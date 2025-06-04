98b03b5cdeb2f7496f3907561b46bb2ffc6c9991
idrury <isaac@lightworksproductions.au>
2025-06-04 15:54:46 +0930

    Final commit

---
+++ Submission/src/cs_assignment_2/Main.java
@@ -0,0 +1,10 @@
+package cs_assignment_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Runner runner = new Runner();
+		runner.start();
+	}
+
+}
--- src/cs_assignment_2/ComputationalModel.java
+++
@@ -1,6 +0,0 @@
-package cs_assignment_2;
-
-public class ComputationalModel {
-
-	
-}
